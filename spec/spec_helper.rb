21e827376ededaf83b09fd42ca460824fc409f10
Ryan Workman <rdavid1099@users.noreply.github.com>
2019-10-15 15:55:15 -0700

    Add framework and gems for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require "bundler/setup"
+require "webmock/rspec"
+require "pry"
+require "timecop"
 require "comiclist"
+
+CURRENT_DAY = Time.local(2019, 10, 13)
+PAST_DAY = Time.local(2019, 5, 7)
+FUTURE_DAY = Time.local(2019, 12, 4)
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -10,5 +19,13 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each) do
+    stub_request(:get, %r{comiclist.com})
+      .to_return(
+        lambda { |req| File.new("./spec/fixtures/list_for_#{req.uri.to_s.split("-")[-3..-1].join("-")}.txt") }
+      )
+    Timecop.freeze(CURRENT_DAY)
   end
 end
